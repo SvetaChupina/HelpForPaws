f55d5211f1bf9abd69b9174a00ce1a7572ff1231
SvetaChupina <167569274+SvetaChupina@users.noreply.github.com>
2024-05-30 17:31:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function how() {
+    document.getElementById("howDel").setAttribute("style", "opacity:1; transition: 1s;");
+    document.getElementById("how").setAttribute("style", "display: none");
+    document.getElementById("howActive").setAttribute("style", "display: block"); 
+    }
+function howActive() {
+    document.getElementById("howDel").setAttribute("style", "display: none");
+    document.getElementById("howActive").setAttribute("style", "display: none");
+    document.getElementById("how").setAttribute("style", "display: block");
+    }
+function recom() {
+    document.getElementById("recomDel").setAttribute("style", "opacity:1; transition: 1s;");
+    document.getElementById("recom").setAttribute("style", "display: none");
+    document.getElementById("recomActive").setAttribute("style", "display: block");
+    }      
+function recomActive() {
+    document.getElementById("recomDel").setAttribute("style", "display: none");
+    document.getElementById("recomActive").setAttribute("style", "display: none");
+    document.getElementById("recom").setAttribute("style", "display: block");
+    }
+function shPet() {
+    document.getElementById("shPetDel").setAttribute("style", "opacity:1; transition: 1s;");
+    document.getElementById("shPet").setAttribute("style", "display: none");
+    document.getElementById("shPetActive").setAttribute("style", "display: block");
+    }
+function shPetActive() {
+    document.getElementById("shPetDel").setAttribute("style", "display: none");
+    document.getElementById("shPetActive").setAttribute("style", "display: none");
+    document.getElementById("shPet").setAttribute("style", "display: block");
+    }
